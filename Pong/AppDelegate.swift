b6996456b10d032000ed6d9b6734eab9da3f9218
Sally <Sally@172-16-195-93.dynapool.nyu.edu>
2018-06-20 15:56:04 -0400

    Committing all files test

--- Pong/AppDelegate.swift
+++ Pong/AppDelegate.swift
@@ -13,7 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+// another testline to commit
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- Pong/GameViewController.swift
+++ Pong/GameViewController.swift
@@ -14,6 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //testline3
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
